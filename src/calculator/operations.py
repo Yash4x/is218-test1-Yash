9f5711471de33da033a96f8373112cbf9a9451db
Yash Mandal <imyashmandal@gmail.com>
2025-09-30 15:20:38 -0400

    feature: implemented add() function with unit tests

---
+++ src/calculator/operations.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    """
+    Add two numbers and return the result.
+
+    Args:
+        a: First number
+        b: Second number
+
+    Returns:
+        The sum of a and b
+    """
+    return a + b
